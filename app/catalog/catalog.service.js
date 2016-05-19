0f1d2217fdb2c9a27c1ef0faa90719b8d6b23979
Mateusz <mateuszrorat@gmail.com>
2016-05-19 22:56:13 +0200

    one minute cache

--- app/catalog/catalog.service.js
+++ app/catalog/catalog.service.js
@@ -1,14 +1,32 @@
 angular.module('szpeje.catalog')
-    .service('SzpejeService', ['$http', function ($http) {
+    .service('SzpejeService', ['$http', '$cacheFactory', '$q', function ($http, $cacheFactory, $q) {
 
         var _self = this;
 
+        var cache = $cacheFactory('dataCache');
+
             this.getSzpeje = function() {
+
+              var ONE_MINUTE = 1 * 60 * 1000; /* ms */
+
+              var cacheId = 'szpeje';
+              var cachedData = cache.get(cacheId);
+              var promise
+              if (cachedData && (new Date() - cachedData.date) < ONE_MINUTE) {
+                console.log('old')
+                 var deferred = $q.defer();
+                 deferred.resolve({data:cachedData});
+                 return deferred.promise;
+              }
+              console.log('new')
               var user = 'DelikatesyProjektowe';
               var apiKey = 'ONmbhVbzOEFvhmBHVfkOZfqLN4SX8FIz';
               var url = 'http://behance.net/v2/users/'+ user +'/projects?api_key='+ apiKey +'&callback=JSON_CALLBACK';
-              var promise = $http.jsonp(url).error(function (response, status) {
+              promise = $http.jsonp(url).error(function (response, status) {
                 alert(status);
+              }).success(function(data, status, headers, config) {
+                data.date = new Date();
+                cache.put(cacheId, data);
               });
               return promise;
             }
