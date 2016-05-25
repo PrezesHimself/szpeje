8bc899306a4740fbc0ea292b8a8476ff8ed7c3ce
Mateusz <mateuszrorat@gmail.com>
2016-05-25 16:34:01 +0200

    basic auth

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ var app = express();
 var compression = require('compression');
 var fallback = require('express-history-api-fallback');
 var root = __dirname + '/dist';
+var auth = require('basic-auth');
 
-app.use(compression())
+app.use(function(req, res, next) {
+    var user = auth(req);
+
+    if (user === undefined || user['name'] !== 'szpeje' || user['pass'] !== 'I said good day!') {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+        res.end('Unauthorized');
+    } else {
+        next();
+    }
+});
 
 app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(root))
-app.use(fallback('index.html', { root: root }))
+app.use(compression());
+app.use(express.static(root));
+app.use(fallback('index.html', { root: root }));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
