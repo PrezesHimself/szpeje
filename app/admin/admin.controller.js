d9cee4fe035e7df6579f9315f88f5b14c8a0d03f
Rorat <roratm@emea.roche.com>
2016-06-04 14:22:25 +0200

    behance 'szpeje' plugged instead of delikatesy, synchronization WIP

--- app/admin/admin.controller.js
+++ app/admin/admin.controller.js
@@ -2,11 +2,26 @@
 
 (function() {
 
-    function AdminController() {
-        var _self = this;
+    function AdminController($q, BehanceApi) {
+        var vm = this;
+
+        vm.synchronize = synchronize;
+        vm.save = save;
+
+        function synchronize() {
+            BehanceApi.getSzpeje()
+                .then(function(results) {
+                    vm.projects = results.data.projects;
+                });
+        }
+
+        function save() {
+            console.log(save);
+        }
+
     }
 
-    AdminController.$inject = []
+    AdminController.$inject = ['$q', 'BehanceApi']
 
     angular.module('szpeje.admin')
         .controller('AdminController', AdminController);
---
+++ app/behanceApi/behanceApi.service.js
@@ -0,0 +1,36 @@
+angular.module('szpeje.behanceApi', [])
+    .service('BehanceApi', ['$http', '$q', function ($http, $q) {
+
+        var _self = this;
+
+
+        var user = 'szpeje';
+        var apiKey = 'ONmbhVbzOEFvhmBHVfkOZfqLN4SX8FIz';
+
+        this.getUser = function() {
+            var url = 'http://www.behance.net/v2/users/'+ user +'?api_key='+ apiKey +'&callback=JSON_CALLBACK';
+            promise = $http.jsonp(url).error(function (response, status) {
+                alert(status);
+            });
+
+            return promise;
+        }
+        this.getProject = function(projectId) {
+            var url = 'http://behance.net/v2/projects/'+ projectId +'?api_key='+ apiKey +'&callback=JSON_CALLBACK';
+            promise = $http.jsonp(url).error(function (response, status) {
+                alert(status);
+            });
+
+            return promise;
+        }
+
+        this.getSzpeje = function() {
+          var url = 'http://behance.net/v2/users/'+ user +'/projects?api_key='+ apiKey +'&callback=JSON_CALLBACK';
+          return $http.jsonp(url).error(function (response, status) {
+            alert(status);
+          }).success(function(data, status, headers, config) {
+            data.date = new Date();
+          });
+        }
+
+    }]);
