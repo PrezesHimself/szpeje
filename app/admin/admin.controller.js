55719cac532001776f847216e82f53924e024093
Rorat <roratm@emea.roche.com>
2016-06-05 09:09:29 +0200

    local api enhanced

--- app/admin/admin.controller.js
+++ app/admin/admin.controller.js
@@ -9,10 +9,27 @@
         vm.save = save;
 
         function synchronize() {
-            BehanceApi.getSzpeje()
+            $q.all([BehanceApi.getSzpeje(), SzpejeApi.getSzpeje()])
                 .then(function(results) {
-                    vm.projects = results.data.projects;
-                    SzpejeApi.deleteSzpeje();
+                    vm.localProjects = _.chain(results[1].data)
+                                        .groupBy('id')
+                                        .value();
+
+                    vm.remoteProjects = angular.copy(_.chain(results[0].data.projects)
+                                        .groupBy('id')
+                                        .value());
+
+                    vm.projects = _.map(results[0].data.projects, function(item){
+                        item.local = vm.localProjects[item.id] ? vm.localProjects[item.id][0] : null;
+                        item.remote = vm.remoteProjects[item.id][0];
+                        item.setRemoteValue = function(key) {
+                            item[key] = item.remote[key];
+                        }
+                        for(var key in item){
+                            item[key] = item.local && item.local[key] ? item.local[key] : item[key]
+                        }
+                        return item;
+                    });
                 });
         }
 
@@ -21,7 +38,17 @@
         }
 
         function save() {
-            console.log(save);
+            SzpejeApi.deleteSzpeje()
+                .then(function(){
+                    var payload = _.map(vm.projects, function(item) {
+                        return {
+                            name:   item.name,
+                            url:    item.url,
+                            id:     item.id
+                        };
+                    });
+                    SzpejeApi.insertSzpeje(payload);
+                });
         }
 
     }
