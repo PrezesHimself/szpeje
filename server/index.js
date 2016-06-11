22e52e7c9b00d0b8b68e59fc08c3cf8266845f4e
Rorat <roratm@emea.roche.com>
2016-06-11 19:32:12 +0200

    basic auth off, index page set

--- server/index.js
+++ server/index.js
@@ -3,24 +3,24 @@ var app = module.exports = express();
 var compression = require('compression');
 var fallback = require('express-history-api-fallback');
 var root = './dist';
-var auth = require('basic-auth');
+// var auth = require('basic-auth');
 
 var bodyParser = require('body-parser');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-    var user = auth(req);
-
-    if (user === undefined || user['name'] !== 'szpeje' || user['pass'] !== 'goodday') {
-        res.statusCode = 401;
-        res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
-        res.end('Unauthorized');
-    } else {
-        next();
-    }
-});
+// app.use(function(req, res, next) {
+//     var user = auth(req);
+//
+//     if (user === undefined || user['name'] !== 'szpeje' || user['pass'] !== 'goodday') {
+//         res.statusCode = 401;
+//         res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+//         res.end('Unauthorized');
+//     } else {
+//         next();
+//     }
+// });
 
 app.set('port', (process.env.PORT || 5000));
 app.use(compression());
