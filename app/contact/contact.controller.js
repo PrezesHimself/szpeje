b6c86330139bb57fdf247124896a5d58118e10af
Rorat <roratm@emea.roche.com>
2016-07-30 12:49:43 +0200

    map added but not precise

--- app/contact/contact.controller.js
+++ app/contact/contact.controller.js
@@ -6,13 +6,47 @@
         var vm = this;
 
         vm.subject = $stateParams.subject;
-        console.log(google.maps, 'test');
+
         var mapDiv = document.getElementById('map');
-        console.log(mapDiv);
-        var map = new google.maps.Map(mapDiv, {
-            center: {lat: 44.540, lng: -78.546},
-            zoom: 8
-        });
+
+        geocoder = new google.maps.Geocoder();
+
+        var latlng = new google.maps.LatLng(50.0701868, 20.0412981);
+        var mapOptions = {
+            zoom: 15,
+            center: latlng
+        };
+
+        var map = new google.maps.Map(mapDiv, mapOptions);
+
+        google.maps.event.addListenerOnce(map, 'idle', codeAddress);
+
+        function codeAddress() {
+
+            // Define address to center map to
+            var address = 'os. Centrum E1, Kraków, Nowa Huta ';
+
+            geocoder.geocode({
+                'address': address
+            }, function (results, status) {
+
+                if (status == google.maps.GeocoderStatus.OK) {
+
+                    // Center map on location
+                    map.setCenter(results[0].geometry.location);
+
+                    // Add marker on location
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: results[0].geometry.location
+                    });
+
+                } else {
+
+                    alert("Geocode was not successful for the following reason: " + status);
+                }
+            });
+        }
     }
 
     ContactController.$inject = ['SendGrid', '$stateParams']
