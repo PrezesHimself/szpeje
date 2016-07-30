30c10de6ab73beec8e9cc5ffe6e5b0d0fce216e0
Rorat <roratm@emea.roche.com>
2016-07-30 12:13:07 +0200

    map test

--- app/contact/contact.controller.js
+++ app/contact/contact.controller.js
@@ -5,7 +5,13 @@
     function ContactController(SendGrid, $stateParams) {
         var vm = this;
 
-        vm.subject = $stateParams.subject; 
+        vm.subject = $stateParams.subject;
+        console.log(google.maps, 'test');
+        var mapDiv = document.getElementById('map');
+        var map = new google.maps.Map(mapDiv, {
+            center: {lat: 44.540, lng: -78.546},
+            zoom: 8
+        });
     }
 
     ContactController.$inject = ['SendGrid', '$stateParams']
