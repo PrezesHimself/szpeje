f8ce21171ca2af7cefd829d8fa1ce39345cbd828
Rorat <roratm@emea.roche.com>
2016-07-30 12:24:25 +0200

    API test

--- app/contact/contact.controller.js
+++ app/contact/contact.controller.js
@@ -8,6 +8,7 @@
         vm.subject = $stateParams.subject;
         console.log(google.maps, 'test');
         var mapDiv = document.getElementById('map');
+        console.log(mapDiv);
         var map = new google.maps.Map(mapDiv, {
             center: {lat: 44.540, lng: -78.546},
             zoom: 8
